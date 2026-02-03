742e3cc49d6e3da924e2352c6003d9da16f91d88
adibaluch70 <adibaluch70@eng.rizvi.edu.in>
2026-02-03 21:26:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-# AIM: Write a Python program to calculate the simple interest based on user input.
-# Coder:
-# Date:
+# AIM: Calculating Simple Interest
+# Coder: Adi Baluch
+# Class = F.E.Computers 1
+# UIN/Roll no = 251P012 / 11
+# Date: 16-01-2026
 
-# Write your code here
+PA = input('Enter Principle Amount: ')
+rate = input('Enter Rate of Interest: ')
+TP = input('Enter Time Period in Years: ')
+SI = (float(PA) * float(rate) * float(TP)) / 100
+f"SI = {SI:.2f}"
+print('\nSimple Interest:',float(SI))
